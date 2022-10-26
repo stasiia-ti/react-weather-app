a0e8e18685750b94a6a67b9c9bd08792c9369c59
Stasia Titovska <patrapema@gmail.com>
2022-10-26 03:24:13 +0300

    App working

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Tokyo" />
       <footer>
-        <p>This project was coded by Anastasiia Titovska <a href='https://github.com/stasiia-ti/my-awsome-app' target="_blank" rel="noreferrer">Open-sourced on GitHub</a></p>
+        <p>This project was coded by Anastasiia Titovska <a href='https://github.com/stasiia-ti/react-weather-app' target="_blank" rel="noreferrer">Open-sourced on GitHub</a></p>
       </footer>
       </div>
     </div>
